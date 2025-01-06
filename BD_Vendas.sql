3c86cdce5e8db2a233dc13190f1bdcdc43692378
Gustavo Martins da Silva <105465730+gu-stavo@users.noreply.github.com>
2025-01-06 12:41:36 -0300

    Update BD_Vendas.sql

--- BD_Vendas.sql
+++ BD_Vendas.sql
@@ -57,21 +57,21 @@ create table item_do_pedido
 /* Inserindo Dados na Tabela Cliente*/
 
 insert into cliente
-values (720,'Ana','Rua 17 n. 19','Niterói','24358310','RJ','12113231/0001-34','2134');
+values (720,'Ana','Rua 17 n. 19','NiterÃ³i','24358310','RJ','12113231/0001-34','2134');
 
 insert into cliente
-values (870,'Flávio','Av Pres. Vargas 10', 'São Paulo','22763931','SP','22534126/9387-9','4631');
+values (870,'FlÃ¡vio','Av Pres. Vargas 10', 'SÃ£o Paulo','22763931','SP','22534126/9387-9','4631');
 
 insert into cliente (codigo_cliente,nome_cliente,endereco,cidade,cep,uf,cgc)
 values (110,'Jorge','Rua Caiapo 13','Curitiba','30078500','PR','14512764/9834-9');
 
 insert into cliente
-values (222,'Lúcia','Rua Itabira 123 Loja 9','Belo 
+values (222,'LÃºcia','Rua Itabira 123 Loja 9','Belo 
 
 Horizonte','22124391','MG','28315213/9348-8','2985');
 
 insert into cliente
-values (830,'Maurício','Av.Paulista 1236 sl/2345','São Paulo','3012683','SP','32816985/7465-6','9343');
+values (830,'MaurÃ­cio','Av.Paulista 1236 sl/2345','SÃ£o Paulo','3012683','SP','32816985/7465-6','9343');
 
 insert into cliente
 values (130,'Edmar','Rua da Praia sn/','Salvador','30079300','BA','23463284/234-9','7121');
@@ -80,46 +80,46 @@ insert into cliente
 values (410,'Rodolfo','Largo da Lapa 27 sobrado','Rio de Janeiro','30078900','RJ','12835128/2346-9','7431');
 
 insert into cliente
-values (20,'Beth','Av. Climério n. 45','São Paulo','25679300','SP','32485126/7326-8','9280');
+values (20,'Beth','Av. ClimÃ©rio n. 45','SÃ£o Paulo','25679300','SP','32485126/7326-8','9280');
 
 insert into cliente (codigo_cliente,nome_cliente,endereco,cidade,uf,cgc,ie)
 values (157,'Paulo','Tv Moraes c/3','Londrina','PR','32848223/324-2','1923');
 
 insert into cliente (codigo_cliente,nome_cliente,endereco,cidade,cep,uf,cgc)
-values (180,'Lívio','Av. Beira Mar n. 1256','Florianópolis','30077500','SC','12736571/2347-4');
+values (180,'LÃ­vio','Av. Beira Mar n. 1256','FlorianÃ³polis','30077500','SC','12736571/2347-4');
 
 insert into cliente
-values (260,'Susana','Rua Lopes Mendes n. 12','Niterói','30046500','RJ','21763571/232-9','2530');
+values (260,'Susana','Rua Lopes Mendes n. 12','NiterÃ³i','30046500','RJ','21763571/232-9','2530');
 
 insert into cliente
-values (290,'Renato','Rua Meireles n. 123 bl.2 sl.345','São Paulo','30225900','SP','13276571/1231-4','1820');
+values (290,'Renato','Rua Meireles n. 123 bl.2 sl.345','SÃ£o Paulo','30225900','SP','13276571/1231-4','1820');
 
 insert into cliente
-values (390,'Sebastião','Rua da Igreja n. 10','Uberaba','30438700','MG','32176547/213-3','9071');
+values (390,'SebastiÃ£o','Rua da Igreja n. 10','Uberaba','30438700','MG','32176547/213-3','9071');
 
 insert into cliente
-values (234,'José','Quadra 3 bl. 3 sl. 1003','Brasília','22841650','DF','21763576/1232-3','2931');
+values (234,'JosÃ©','Quadra 3 bl. 3 sl. 1003','BrasÃ­lia','22841650','DF','21763576/1232-3','2931');
 
 
 /* Inserindo Dados na Tabela Vendedor*/
 
-insert into vendedor values (209,'José',1800.00,'C');
+insert into vendedor values (209,'JosÃ©',1800.00,'C');
 
 insert into vendedor values (111,'Carlos',2490.00,'A');
 
-insert into vendedor values (11,'João',2780.00,'C');
+insert into vendedor values (11,'JoÃ£o',2780.00,'C');
 
-insert into vendedor values (240,'Antônio',9500.00,'C');
+insert into vendedor values (240,'AntÃ´nio',9500.00,'C');
 
 insert into vendedor values (720,'Felipe',4600.00,'A');
 
 insert into vendedor values (213,'Jonas',2300.00,'A');
 
-insert into vendedor values (101,'João',2650.00,'C');
+insert into vendedor values (101,'JoÃ£o',2650.00,'C');
 
 insert into vendedor values (310,'Josias',870.00,'B');
 
-insert into vendedor values (250,'Maurício',2930.00,'C');
+insert into vendedor values (250,'MaurÃ­cio',2930.00,'C');
 
 
 /* Inserindo Dados na Tabela Pedido*/
@@ -172,7 +172,7 @@ insert into produto values (78,'L','Vinho',2.00);
 
 insert into produto values (22,'M','Linho',0.11);
 
-insert into produto values (30,'SAC','Açúcar',0.30);
+insert into produto values (30,'SAC','AÃ§Ãºcar',0.30);
 
 insert into produto values (53,'M','Linha',1.80);
 
@@ -248,12 +248,3 @@ insert into item_do_pedido values (119,22,10);
 insert into item_do_pedido values (119,53,43);
 
 insert into item_do_pedido values (137,13,8);
-
-
-
-/* Salvando os Dados Inseridos */
-
-/*commit;*/
-
-
-/* Fim */
